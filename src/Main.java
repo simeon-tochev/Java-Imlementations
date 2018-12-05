02ff61ea621abf3774d409a1ce6dfd2112085fbd
simeon-tochev <simeon_tochev98@abv.bg>
2018-12-05 21:14:01 +0200

    Minor tweaks

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 import structures.Stack;
 import structures.Queue;
 import structures.ArrayList;
+import structures.LinkedList;
 
 public class Main {
 	
-	static ArrayList<Integer> arr = new ArrayList<Integer>();
+	static LinkedList<Integer> list = new LinkedList<Integer>();
 	
 	public static void main(String[] args) {
-		
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(5);
+		list.add(6);
+		list.addAt(4, 4);
+		// System.out.println(list.get(2));
+		for(Integer i : list) {
+			System.out.println(i);
+		}
 	}
 
 }
--- src/structures/LinkedList.java
+++ src/structures/LinkedList.java
@@ -28,7 +28,32 @@ public class LinkedList<T> implements Cloneable, Iterable<T> {
 	}
 	
 	public void add(T el) {
+		if(isEmpty()) {
+			head = new Node<T>(el);
+			size ++;
+			return;
+		}
 		getLastNode().next = new Node<T>(el);
+		size++;
+	}
+	
+	public void addAt(T el, int index) {
+		if(index == 0) {
+			Node<T> n = new Node<T>(el, head);
+			head = n;
+			size++;
+			return;
+		}
+		Node<T> curr = head;
+		for(int i = 1; i < index; i++) {
+			if(curr.next == null) {
+				return;
+			}
+			curr = curr.next;
+		}
+		Node<T> n = new Node<T>(el, curr.next);
+		curr.next = n;
+		size++;
 	}
 	
 	public T get(int index) {
@@ -45,12 +70,32 @@ public class LinkedList<T> implements Cloneable, Iterable<T> {
 		return getLastNode().data;
 	}
 	
+	public void removeLast() {
+		if(head == null) {
+			return;
+		} else if (head.next == null) {
+			head = null;
+			return;
+		}
+		Node<T> curr = head;
+		while(curr.next.next != null) {
+			curr = curr.next;
+		}
+		curr.next = null;
+		size--;
+		
+	}
+	
 	public boolean contains(T el) {
 		return false;
 	}
 	
 	public boolean isEmpty() {
 		return head == null;
+	}
+	
+	public int getSize() {
+		return size;
 	}
 	
 	private Node<T> getLastNode(){
@@ -81,11 +126,11 @@ public class LinkedList<T> implements Cloneable, Iterable<T> {
 	private class LinkedListIterator<T> implements Iterator<T> {
 		
 		@SuppressWarnings("unchecked")
-		private Node<T> curr = new Node<T>(null, (Node<T>) head);
+		private Node<T> curr = (Node<T>) head;
 		
 		@Override
 		public boolean hasNext() {
-			return curr.next != null;
+			return curr != null;
 		}
 		
 		@Override
