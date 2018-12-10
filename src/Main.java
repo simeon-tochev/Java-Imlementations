0f046ba07bad77efed02ab73f045ea650294347c
simeon-tochev <simeon_tochev98@abv.bg>
2018-12-10 14:21:55 +0200

    Insertion Sort

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import algorithms.sorting.BubbleSort;
+import algorithms.sorting.*;
 
 public class Main {
 	
 	static int[] arr = new int[] {5, 8, 12, 2, 0, 0, 16, 2, 3, -23, 4, 6};
 	
 	public static void main(String[] args) {
-		int[] sarr = BubbleSort.sort(arr);
+		int[] sarr = InsertionSort.sort(arr);
 		for(int k: sarr) {
 			System.out.println(k);
 		}
--- src/algorithms/sorting/InsertionSort.java
+++ src/algorithms/sorting/InsertionSort.java
@@ -7,9 +7,16 @@ public class InsertionSort {
 		int[] sarr = new int[len];
 		int k = 0;
 		for(int i = 0; i < len; i++) {
-			for(int j = k - 1; j >= 0; j++) {
-				
+			sarr[k] = arr[i];
+			for(int j = k - 1; j >= 0; j--) {
+				if(sarr[j+1] < sarr[j]) {
+					int x = sarr[j];
+					sarr[j] = sarr[j+1];
+					sarr[j+1] = x;
+				} else break;
 			}
+			k++;
 		}
+		return sarr;
 	}
 }
